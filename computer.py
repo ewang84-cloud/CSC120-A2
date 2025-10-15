4f9cd05a75b705807e3d91c0b1e3bc454bcf9afb
ewang84-cloud <ewang84@smith.edu>
2025-10-15 12:20:16 -0400

    revise 1

--- computer.py
+++ computer.py
@@ -1,18 +1,45 @@
 class Computer:
-    name: str
+    description: str
+    processor_type: str
+    hard_drive_capacity: str
+    memory: str
+    operating_system: str
     price: float
+    year_made: int
     OS: str
+    
 
-    def __init__(self, name, price, OS):
-        self.name = name
+    def __init__(self, description, processor_type, hard_drive_capacity, memory, operating_system, year_made, price, OS):
+        self.description = description
+        self.processor_type = processor_type
+        self.hard_drive_capacity = hard_drive_capacity
+        self.memory = memory
+        self.operating_system = operating_system
+        self.year_made = year_made
         self.price = price
         self.OS = OS
 
+
     def getName(self):
-        print (self.name)
+        print (self.description)
 
     def getPrice(self):
         print (self.price)
+
+    def getProcessorType(self):
+        print (self.processor_type)
+
+    def getYearMade(self):
+        print (self.year_made)
+
+    def getHardDriveCapacity(self):
+        print (self.hard_drive_capacity)
+
+    def getMemory(self):
+        print (self.memory)
+
+    def getOperatingSystem(self):
+        print (self.operating_system)
 
     def getOS(self):
         print (self.OS)
